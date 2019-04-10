7bcaaa585efe8610c31ea14eeb0dba1eb0efc0da
Your Name <you@example.com>
2019-04-10 17:52:39 +0100

    Created simple unit test to verify dev environment.

---
+++ game-runner/frotz_runner.py
@@ -0,0 +1,11 @@
+#############################################################################
+# frotz_runner.py
+#----------------------------------------------------------------------------
+# Provides functions that can be used to control frotz running in 'dumb'
+# mode.
+#############################################################################
+
+
+#simple function to verify unit tests are working
+def func ( x ):
+    return x + 1;
