0c9543c491012dcc1a1933b7516bdf6ccbf1b5a8
Your Name <you@example.com>
2019-04-15 15:42:46 +0100

    Print json data.

--- tab_src/twitter_connection.py
+++ tab_src/twitter_connection.py
@@ -4,3 +4,11 @@ import json
 def load_keys ():
 
     print ( "Loading keys..." )
+
+    with open ( "twitter_keys.json" ) as json_data_file :
+
+        data = json.load ( json_data_file ) 
+
+        print ( "Json data:" )
+
+        print ( data )
