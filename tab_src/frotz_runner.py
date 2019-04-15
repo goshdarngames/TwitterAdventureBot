469e0c7c9d97de45663e6693f154d7f35f72651d
Your Name <you@example.com>
2019-04-15 12:09:34 +0100

    Added comments.

--- tab_src/frotz_runner.py
+++ tab_src/frotz_runner.py
@@ -3,6 +3,11 @@ from subprocess import PIPE, Popen
 from threading import Thread
 from queue import Queue, Empty
 
+#----------------------------------------------------------------------------
+
+#These function are used by the FrotzRunner threads to continuously read
+#output and send commands to the game sub-process
+
 def read_subp_output ( subPstdout, outQueue ):
 
     #keep reading output lines and adding to queue until program finishes
@@ -15,7 +20,17 @@ def write_subp_input ( subPstdin, inQueue ):
 
         subPstdin.write ( inQueue.get () )
 
+#----------------------------------------------------------------------------
+
 class FrotzRunner:
+    """
+    Provides a 'context manager' that can be used to control a dfrotz game.
+
+    The game is run in a sub-process and two threads are used to input
+    commands and read output from the game.
+
+    A demonstration of its usage can be found in the 'frotz_cmd.py' file.
+    """
 
     def __init__ ( self, gamePath ):
 
@@ -59,6 +74,19 @@ class FrotzRunner:
 
     def read_output_line  ( self ):
 
+        """
+        Reads the latest line of output and returns it as a string.
+        
+        Throws Empty if there is no output available but the game 
+        sub-process is still running.
+
+        Throws EOFError when the final line of output is read from 
+        game sub-process.
+
+        Calling this again after the EOFError is undefined.  Use poll()
+        to check the game sub-process is running.
+        """
+
         line = self.outputQueue.get_nowait ()
 
         if line == '':
@@ -72,6 +100,22 @@ class FrotzRunner:
     #------------------------------------------------------------------------
 
     def read_output_block ( self, num_retry = 5, wait_time = 0.1 ):
+        """
+        Tries to read a block of lines from the game sub-process.  
+
+        After reading each line the system will wait and try read again
+        in case there are more lines of output that have been delayed by
+        the game sub-process.
+
+        This function will return a list containing all the lines of
+        output.
+
+        No error will be thrown when the process is closed so it is 
+        important to call poll() to ensure the game is still alive.
+
+        It was designed not to throw an error so that the 'quit' message
+        from the game would not be lost.
+        """
 
         retries = num_retry
 
@@ -101,17 +145,29 @@ class FrotzRunner:
     #------------------------------------------------------------------------
 
     def write_command ( self, cmd ):
+        """
+        Sends a command to the game.
+        """
 
         self.inputQueue.put ( cmd )
 
     #------------------------------------------------------------------------
 
     def poll ( self ):
+        """
+        Returns None if the game sub-process is running.
+
+        If the game sub-process has finished it returns the returncode
+        of the sub-process.
+        """
 
         return self.subP.poll ()
 
     #------------------------------------------------------------------------
 
     def return_code ( self ):
+        """
+        Returns the returncode of the game sub-process if it has finished.
+        """
 
         return self.subP.returncode
