a9af8b62e9cef3ce598aa9f438ada2e9f995fc0e
Your Name <you@example.com>
2019-04-22 12:47:24 +0100

    Created test for chop_text.

--- tab_src/twitter_connection_test.py
+++ tab_src/twitter_connection_test.py
@@ -4,3 +4,30 @@
 # Unit tests for the twitter connection.
 #############################################################################
 
+from twitter_connection import *
+
+def test_chop_text ():
+
+    testCases = \
+    [
+        {
+            "txt" : "aabbccdd",
+            "n"   : 2,
+            "out" : [ "aa", "bb", "cc", "dd" ]
+        }
+    ]
+
+    for tc in testCases:
+
+        out = chop_text ( tc [ "txt" ], tc [ "n" ] )
+
+        #check that the output ( list of strings ) is equal to the output
+        #expected in the test case
+
+        assert ( len ( out ) == len ( tc [ "out" ] ) )
+
+        for i in range ( len ( out ) ):
+
+            assert ( out [ i ] == tc [ "out" ] [ i ] )
+
+
