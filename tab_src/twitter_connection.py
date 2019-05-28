cb682dba05231aec497e36fd42c2ca6729c3dd82
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-05-28 17:53:42 +0100

    missing comma

--- tab_src/twitter_connection.py
+++ tab_src/twitter_connection.py
@@ -196,7 +196,7 @@ class TwitterConnection:
 
             time.sleep ( TWEEP_RATE_ERROR_SLEEP )
 
-        except ( SSLError, Timeout, ConnectionError, OSError
+        except ( SSLError, Timeout, ConnectionError, OSError,
                  NewConnectionError, tweepy.TweepError ) as e:
 
             logging.warning ( "Network Error:  ", str ( e ) )
